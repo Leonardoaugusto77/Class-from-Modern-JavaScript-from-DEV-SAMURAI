76a47b89ff76de6e76c03e849457a3ebb640a1f9
Heisen <leonardopjh30@gmail.com>
2024-02-13 21:57:57 -0300

    frist(lesson): created example from strucy js

--- first lesson/Javascript.js
+++ first lesson/Javascript.js
@@ -3,3 +3,21 @@ var nameofUser = "leo";
 console.log(`${nameofUser}`);
 
 /* Comentarios */
+
+// Ask for the user's name
+let userName = window.prompt("What's your name, bro?");
+
+// Ask for the user's age
+let age = window.prompt("What's your age?");
+
+// Convert the age to a number
+age = parseInt(age);
+
+// Check if the age is less than or equal to 18
+if (age <= 18) {
+  // Display a message if the user is under 18
+  window.alert("Sorry, you don't have 18 years.");
+} else {
+  // Display a welcome message if the user is 18 or older
+  window.alert("Hello, welcome to the site!");
+}
--- second lesson/index.js
+++ second lesson/index.js
@@ -1,3 +1,5 @@
 console.log("1", typeof 1);
 
 console.log("Text", typeof "text"); // In this case, typeof returns a string type if we pass the text in ""
+
+console.log(2 + 2);
